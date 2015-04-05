5e9d1d0af92092eb0be221625c52a057db12512c
Liwei Sun <sunliwei.china@gmail.com>
2015-04-05 16:39:59 -0400

    Just Finished

--- factorial.py
+++ factorial.py
@@ -8,17 +8,24 @@ from nose.tools import assert_equal
 
 def factorial_recursive(n):
     # TODO Define your logic for factorial here
-    pass # does nothing
+    if n == 1:
+        return n
+    else:
+        return n*factorial_recursive(n-1)
 
 def test_factorial():
     assert_equal(factorial_recursive(1), 1)
     # TODO: add more
+    assert_equal(factorial_recursive(2), 2)
+    assert_equal(factorial_recursive(3), 6)
+    assert_equal(factorial_recursive(4), 24)
+    assert_equal(factorial_recursive(5), 120)
 
 if __name__ == '__main__':
     # This is a way to determine either file was "executed", so if it was
     # imported (by e.g. nose) as a library, we should not run code
     # below
 
-    nconditions = raw_input("Please enter number of conditions: ")
+    nconditions = long(raw_input("Please enter number of conditions: "))
     norders = factorial_recursive(nconditions)
-    print("Number of possible trial orders: " + str(norders)
+    print("Number of possible trial orders: " + str(norders))
